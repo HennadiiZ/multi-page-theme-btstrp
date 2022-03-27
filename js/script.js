f3cf5190ba853032cf245aeb15376f6dc66618ac
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-03-27 19:59:23 +0300

    VIDEO PLAY + VIDEO MODAL (is not running as expected)

--- js/script.js
+++ js/script.js
@@ -34,3 +34,31 @@ $('.carousel').carousel({
   // pause: 'hover',
   // wrap: true,
 })
+
+
+// VIDEO MODAL
+//Video Play
+$(function() {
+  //Audio Play modal video 
+  $(".video").click(function(){
+    let theModal = $(this).data("target"),
+    videoSRC = $(this).attr("data-video"),
+    videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+    $(theModal + " iframe").attr('src', videoSRCauto);
+    $(theModal + " button.close").click(function(){
+      $(theModal + " iframe").attr('src', videoSRC);
+    });
+  });
+});
+
+// $(function() {
+//   $(".video").click(function () {
+//     var theModal = $(this).data("target"),
+//     videoSRC = $(this).attr("data-video"),
+//     videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+//     $(theModal + ' iframe').attr('src', videoSRCauto);
+//     $(theModal + ' button.close').click(function () {
+//       $(theModal + ' iframe').attr('src', videoSRC);
+//     });
+//   });
+// });
